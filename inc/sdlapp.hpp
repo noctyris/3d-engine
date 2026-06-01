272eca7daf831f684fd9eb2755d5af29a0636ca0
Noctyris <noctyris@proton.me>
2026-06-01 20:15:49 +0200

    Delete overtabs

--- inc/sdlapp.hpp
+++ inc/sdlapp.hpp
@@ -1,43 +1,45 @@
 #ifndef SDLAPP_HPP
 #define SDLAPP_HPP
 
+#define FPS 60
+
 #include <SDL3/SDL.h>
 #include <string>
 
 struct Color {
-    Uint8 r, g, b, a;
+  Uint8 r, g, b, a;
 
-    Color(Uint8 r = 0, Uint8 g = 0, Uint8 b = 0, Uint8 a = 255)
-        : r(r), g(g), b(b), a(a) {}
+  Color(Uint8 r = 0, Uint8 g = 0, Uint8 b = 0, Uint8 a = 255)
+    : r(r), g(g), b(b), a(a) {}
 };
 
 class Vec2 {
   public:
-    int x, y;
-    Vec2(int x, int y) : x(x), y(y) {};
+  int x, y;
+  Vec2(int x, int y) : x(x), y(y) {};
 };
 
 class SDLApp {
 public:
-    SDLApp(const std::string& title, int width, int height);
-    ~SDLApp();
+  SDLApp(const std::string& title, int width, int height);
+  ~SDLApp();
 
-    // Prevent copying because of the raw pointers (RAII)
-    SDLApp(const SDLApp&) = delete;
-    SDLApp& operator=(const SDLApp&) = delete;
+  // Prevent copying because of the raw pointers (RAII)
+  SDLApp(const SDLApp&) = delete;
+  SDLApp& operator=(const SDLApp&) = delete;
 
-    bool isRunning() const { return m_running; }
-    void pollEvents();
-    void clear(const Color& color);
-    void drawLine(Vec2 pos1, Vec2 pos2, const Color& color);
-    void present();
+  bool isRunning() const { return m_running; }
+  void pollEvents();
+  void clear(const Color& color);
+  void drawLine(Vec2 pos1, Vec2 pos2, const Color& color);
+  void present();
 
 private:
-    SDL_Window* m_window = nullptr;
-    SDL_Renderer* m_renderer = nullptr;
-    bool m_running = true;
+  SDL_Window* m_window = nullptr;
+  SDL_Renderer* m_renderer = nullptr;
+  bool m_running = true;
 
-    void setDrawColor(const Color& color);
+  void setDrawColor(const Color& color);
 };
 
 #endif
