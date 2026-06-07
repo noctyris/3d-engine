2b8ffe57ee2d9a126154f424431c5ad5509ff0a0
Noctyris <noctyris@proton.me>
2026-06-07 21:46:29 +0200

    Define matrix class and methods

--- inc/matrix.hpp
+++ inc/matrix.hpp
@@ -1,6 +1,21 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
+#include <initializer_list>
+#include <iostream>
+#include "vector.hpp"
 
+class Mat4 {
+  public:
+    float values[16];
+
+    Mat4() { for (int i = 0; i < 16; i++) values[i] = 0; };
+    Mat4(std::initializer_list<float> list);
+    float& operator()(int r, int c);
+    float operator()(int r, int c) const;
+    Mat4 operator*(const Mat4& other);
+    Vec4 operator*(const Vec4& v);
+    void print();
+};
 
 #endif
--- matrix.cpp
+++ matrix.cpp
@@ -1,1 +1,50 @@
 #include "matrix.hpp"
+
+Mat4::Mat4(std::initializer_list<float> list) {
+  int i = 0;
+  for (float val : list) {
+    if (i < 16) values[i++] = val;
+  }
+}
+
+float& Mat4::operator()(int r, int c) {
+  return values[r * 4 + c];
+}
+
+float Mat4::operator()(int r, int c) const {
+  return values[r * 4 + c];
+}
+
+Mat4 Mat4::operator*(const Mat4& other) {
+  Mat4 result;
+  for(int i=0; i < 16; i++) result.values[i] = 0.0f;
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      for (int k = 0; k < 4; k++) {
+        result(i, j) += (*this)(i, k) * other(k, j);
+      }
+    }
+  }
+  *this = result;
+  return *this;
+}
+
+Vec4 Mat4::operator*(const Vec4& v) {
+  Vec4 res;
+  for (int i = 0; i < 4; i++) {
+    res.v[i]=0;
+    for (int j = 0; j < 4; j++) {
+      res.v[i] += (*this)(i, j) * v.v[j];
+    }
+  }
+  return res;
+}
+
+void Mat4::print() {
+  for (int r = 0; r < 4; r++) {
+    for (int c = 0; c < 4; c++) {
+      std::cout << (*this)(r, c) << "\t";
+ }
+    std::cout << "\n";
+  }
+}
