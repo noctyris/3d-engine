6acf68292e5fcc98298ba80a6fb48d40c18d50e5
Noctyris <noctyris@proton.me>
2026-06-08 21:09:46 +0200

    Use whole progress so far

--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
 #include "camera.hpp"
+#include "light.hpp"
 #include "mesh.hpp"
 #include "sdlapp.hpp"
+#include "types.hpp"
 #include "vector.hpp"
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 800;
 const std::pair<int, int> DIMS = {SCREEN_WIDTH, SCREEN_HEIGHT};
 
-int main() {
+int main(int argv, char* argc[]) {
   SDLApp sdl("3D Engine C++", SCREEN_WIDTH, SCREEN_HEIGHT);
   SDL_SetWindowRelativeMouseMode(sdl.get_window(), true);
+  float speed = 0.05f;
+  Vec2f mouse;
 
   Camera camera( Vec4(6, 2, 3, 1), Vec4(0, 0, 0, 1), 70.0f*M_PI/180, { 0.01f, 1000.0f }, DIMS );
   camera.gen_projection_matrix();
 
   Mesh mesh;
-  mesh.load_from_obj("models/Untitled.obj");
+  mesh.load_from_obj(argc[1]);
+  mesh.transform.set_scale(Vec4(.01,.01,.01));
   mesh.get_info();
 
-  float speed = 0.05f;
-  Vec2f mouse;
+  std::vector<Light*> lights;
+  lights.push_back(new DirectionalLight(Vec4(1, -1, 1)));
+  lights.push_back(new AmbientLight(Color(255, 255, 255), 0.1f));
+  lights.push_back(new PointLight(Vec4(0, 5, 0), 20.0f, Color(255, 100, 100)));
 
   while (sdl.isRunning()) {
     sdl.pollEvents();
@@ -39,7 +46,7 @@ int main() {
     sdl.clear(Color(0, 0, 0));
     
     mesh.transform.rotate(Vec4(0,1,1/M_PI), M_PI/180);
-    mesh.show(&sdl, camera);
+    mesh.show(&sdl, camera, lights);
 
     sdl.present();
   }
